fd21abc30257b027db51f59844349b6bf0a699ec
KikeX3 <jguerrero24@ucol.mx>
2020-12-08 23:12:26 -0600

    Implemented sumarParesDo

--- src/main.js
+++ src/main.js
@@ -52,6 +52,40 @@ export default class App {
         }
         return suma;
     }
+
+    contarImparesWhile(inicio , fin){
+
+        let i = inicio;
+        let contar = 0; 
+        
+        while(i <= fin){
+
+            if(i % 2 !== 0 ){
+                contar ++;
+            }
+
+            i ++;
+
+        }
+        return contar;
+    }
+
+    sumarParesDo(){
+        
+        let i = 1;
+        let suma = 0;
+        
+        do {
+            if (i%2 === 0){
+                suma = suma + i;
+            }
+          
+            i++;
+        
+        } while(i <= 20);
+
+        return suma;
+    }
 }
 
 let app = new App();
@@ -61,3 +95,7 @@ console.log(app.sumarParesFor());
 console.log(app.contarImparesFor(5, 10));
 
 console.log(app.sumarParesWhile());
+
+console.log(app.contarImparesWhile(5,10));
+
+console.log(app.sumarParesDo());
